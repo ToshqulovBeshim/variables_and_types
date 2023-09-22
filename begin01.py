c2b3277a385ec1edfc9a8faab4d95abb69a6fc97
ToshqulovBeshim <tbeshimboyy@gmail.com>
2023-09-22 09:10:17 +0300

    misol 1

--- begin01.py
+++ begin01.py
@@ -1,5 +1,7 @@
+
 # Assign 5 (int value) to 'num_one' variable;
-
+beshim = 5
 # Assign 4 (int value) to 'num_two' variable;
-
-# Print sum of 'num_one' and 'num_two;
+navruz = 4
+# Print sum of 'num_one' and 'num_two;~
+print( beshim + navruz)
